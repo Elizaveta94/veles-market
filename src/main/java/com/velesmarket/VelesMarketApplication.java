e16d90a0e87599a81de65eb502e5a3c2b03775d8
Elizaveta Smirnova <liza.300994@gmail.com>
2023-03-19 17:19:32 +0300

    Initial commit

---
+++ src/main/java/com/velesmarket/VelesMarketApplication.java
@@ -0,0 +1,13 @@
+package com.velesmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelesMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelesMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/velesmarket/VelesMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velesmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelesMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
